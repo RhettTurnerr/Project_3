30ea02728f134620738d97cbe2471d61dd19411b
Rhett Turner <Rhettster06@gmail.com>
2026-02-09 19:56:58 -0600

    added new base

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/Project_3/",
+  base: "/index.html",
   plugins: [
     react({
       babel: {
